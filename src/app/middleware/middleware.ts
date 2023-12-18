c748e6b4c248bc7db4567f744f41dd37046130a6
ChrisShep98 <cshepard@blockchains.com>
2023-12-17 21:39:45 -0500

    middleware added to prevent non users from seeing only user content? this one seemed weird

---
+++ src/app/middleware/middleware.ts
@@ -0,0 +1,4 @@
+export { default } from "next-auth/middleware"
+
+// below allows it so the dashboard page is not accessable when user is not logged in, add other pages later
+export const config = {matcher: ["/dashboard"]}
