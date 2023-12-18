51edbab315895d842a25cc3f9b5876ea804df401
ChrisShep98 <cshepard@blockchains.com>
2023-12-17 21:42:49 -0500

    next auth api BROKEN, login UI with state and handlers, changed naming

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,54 @@
+import { connectMongoDB } from "@/app/lib/mongodb";
+import User from "@/app/models/Users";
+import { AuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs"
+
+
+const authOptions: AuthOptions = {
+    providers: [
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. 'Sign in with...')
+            name: "credentials",
+            // The credentials is used to generate a suitable form on the sign in page.
+            // You can specify whatever fields you are expecting to be submitted.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+                userName: { label: "Username", type: "text"},
+                password: { label: "Password", type: "password"},
+            },
+            async authorize(credentials: any){
+                try {
+                    await connectMongoDB();
+                   const user = await User.findOne({userName: credentials.userName })
+
+                   if(!user){
+                    return null;
+                   }
+                   const passwordsMatch = await bcrypt.compare(credentials.password, user.password);
+
+                   if (!passwordsMatch) {
+                    return null;
+                  }
+                  return user;
+                } catch (error) {
+                    console.log("Error: ", error)
+                }
+            }
+        })
+    ],
+    session: {
+        strategy: 'jwt'
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+        // the route used to login
+        signIn: '/'
+    }
+};
+
+const handler = NextAuth(authOptions)
+
+export {handler as GET, handler as POST}
--- src/app/components/RegisterForm.tsx
+++ src/app/components/RegisterForm.tsx
@@ -47,7 +47,7 @@ const RegisterForm = () => {
       <Stack justifyContent={'center'} alignItems={'center'} spacing={1}>
       <Typography>Register an account here!</Typography>
       <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column'}}>
-      <TextField onChange={(e) => setUserName(e.target.value)} type='text' placeholder='Full Name'></TextField>
+      <TextField onChange={(e) => setUserName(e.target.value)} type='text' placeholder='User Name'></TextField>
       <TextField onChange={(e) => setEmail(e.target.value)} type='email' placeholder='Email'></TextField>
       <TextField onChange={(e) => setPassword(e.target.value)} type='password' placeholder='Password'></TextField>
       <Button type='submit'>Login</Button>
