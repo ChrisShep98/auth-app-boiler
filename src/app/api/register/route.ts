ef86106ac6fb9b81cf5787bb09937d7f99a49fa5
ChrisShep98 <cshepard@blockchains.com>
2023-12-17 21:41:39 -0500

    commented code from a type issue I think

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -2,10 +2,13 @@ import { connectMongoDB } from "@/app/lib/mongodb";
 import User from "@/app/models/Users";
 import { NextResponse } from "next/server"
 import bcrypt from "bcryptjs"
+import { NextApiRequest, NextApiResponse } from 'next'
 
 export async function POST(req: any){
     try{
         const { userName, email, password } = await req.json()
+        // try out below code when getting req param to NextApiRequest type
+        // const { userName, email, password } = await req.body.json()
 
         const hashedPassword = await bcrypt.hash(password, 10)
         await connectMongoDB();
