69b56280c74ebedbad13c9191134515dc65a5335
ChrisShep98 <cshepard@blockchains.com>
2023-12-17 21:40:48 -0500

    signOut method from next-auth BROKEN currently

--- src/app/components/UserInfo.tsx
+++ src/app/components/UserInfo.tsx
@@ -1,12 +1,14 @@
+"use client";
 import { Button, Stack, Typography } from '@mui/material'
 import React from 'react'
+import { signOut } from 'next-auth/react'
 
 const UserInfo = () => {
   return (
     <Stack>
         <Typography>Name: HARD_CODED_NAME</Typography>
         <Typography>Email: HARD_CODED_EMAIL</Typography>
-        <Button>Logout</Button>
+        <Button onClick={() => signOut()}>Logout</Button>
     </Stack>
   )
 }
